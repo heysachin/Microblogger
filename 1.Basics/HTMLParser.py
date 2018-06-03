d62314a75458cb0eed7a67cf43154d66fc43c568
Sachin <hellosachindev@gmail.com>
2018-06-03 17:22:14 +0530

    Price Change Alerts Console App Done

--- 1.Basics/HTMLParser.py
+++ 1.Basics/HTMLParser.py
@@ -3,7 +3,6 @@
 # Created by Sachin Dev on 26/05/18
 
 
-
 import requests
 from bs4 import BeautifulSoup
 
@@ -13,15 +12,15 @@ request = requests.get("https://www.flipkart.com/omron-hem-8712-bp-monitor/p/itm
 
 content=request.content
 
-soup = BeautifulSoup(content,"html.parser")
+soup = BeautifulSoup(content, "html.parser")
 
-element = soup.find("div",{"class":"_1vC4OE _3qQ9m1"})
+element = soup.find("div", {"class":"_1vC4OE _3qQ9m1"})
 
 element=element.text.strip()
 
 s_string = element[1:]
 
-s_string=s_string.replace(",","")
+s_string=s_string.replace(",", "")
 
 print (int(s_string))
 
---
+++ 4. Price Change Tracker App/src/models/users/constants.py
@@ -0,0 +1,5 @@
+
+
+# Created by Sachin Dev on 01/06/18
+
+COLLECTION = 'items'
