57bf1ac250c2b6835dc11eecbf5548bd7095b64b
Sachin <hellosachindev@gmail.com>
2018-10-11 00:55:10 +0530

    *

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/SSD2/Documents/Python Workplace/Complete-Python-Web-Course-Build-8-Python-Web-Apps/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
